fc5f033212c9479c3fd016d206b57c5fae9d58ec
vahid <vahid.negahdari@gmail.com>
2025-11-17 02:06:52 +0200

    adds build sinks function for sinks

--- crates/deltaforge-sinks/src/lib.rs
+++ crates/deltaforge-sinks/src/lib.rs
@@ -1,2 +1,27 @@
+use std::sync::Arc;
+
+use deltaforge_config::{PipelineSpec, SinkCfg};
+use deltaforge_core::ArcDynSink;
+
+use crate::{kafka::KafkaSink, redis::RedisSink};
+
 pub mod kafka;
 pub mod redis;
+
+pub fn build_sinks(ps: &PipelineSpec) -> Vec<ArcDynSink> {
+    ps.spec.sinks
+        .iter()
+        .map(|s| {
+            match s {
+                SinkCfg::Kafka(kafka_cfg) => {
+                    let sink = KafkaSink::new(kafka_cfg).unwrap(); 
+                    Arc::new(sink) as ArcDynSink
+                },
+                SinkCfg::Redis(redis_cfg) => {
+                    let sink = RedisSink::new(&redis_cfg.uri, &redis_cfg.stream).unwrap(); 
+                    Arc::new(sink) as ArcDynSink
+                },
+            }
+        })
+        .collect()
+}
